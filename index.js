fa1585ed13bc48be833bcb122617d5f1dd3f0198
cpan1995 <cpan1995@gmail.com>
2021-06-13 19:48:34 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const mainElem = document.querySelector('main')
+mainElem.remove();
+
+const newHeader = document.createElement('h1')
+newHeader.id = 'victory'
+newHeader.innerHTML = 'is the champion'
